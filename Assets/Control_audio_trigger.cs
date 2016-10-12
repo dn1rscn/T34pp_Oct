1e3d96879591290da7921cc30ab512fbb67a6ecb
Gorkapromero <drmoriartydiu@gmail.com>
2016-10-12 15:57:46 +0200

    Prueba sonidos islas

---
+++ Assets/Control_audio_trigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control_audio_trigger : MonoBehaviour 
+
+{
+	public AudioClip mush; 
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<AudioSource> ().playOnAwake = false;
+		GetComponent<AudioSource> ().clip = mush;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider coli)
+
+	{
+		GetComponent<AudioSource> ().Play ();
+	}
+
+	void OnTriggerExit(Collider coli)
+
+	{
+		GetComponent<AudioSource> ().Stop ();
+	}
+}
