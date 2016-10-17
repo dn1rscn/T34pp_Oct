8655b6b6870614e25a843c14ae9c3416d941e26d
dn1rscn <dn1.rscn@gmail.com>
2016-10-17 19:09:01 +0200

    Varios_PortalesIslas

--- Assets/0-Pruebas/Dani/CogerPrefabsAvatares/controlPortales.cs
+++ Assets/0-Pruebas/Dani/CogerPrefabsAvatares/controlPortales.cs
@@ -17,7 +17,9 @@ public class controlPortales : MonoBehaviour {
 		//CDG_Mundo3D = GameObject.Find("ControlDatosGlobales").GetComponent<ControlDatosGlobales_Mundo3D>();
 
 		animator_PanelCanvas = GameObject.Find ("CanvasPortal_Verde").GetComponent<Animator> ();
+		print (" animator panelCanvas detectado");
 		animator_botonesPortal = GameObject.Find("botonesPortal").GetComponent<Animator>();
+		print (" animator botonesPortal detectado");
 	}
 	
 	void OnTriggerEnter(Collider coli)
@@ -29,12 +31,13 @@ public class controlPortales : MonoBehaviour {
 			animator_botonesPortal.Play("IntroBotonesPortales");
 
 			print ("Colision con: "+coli.gameObject.name);
+
+			//TRIGGERS ISLA BOSQUE
 			if(coli.gameObject.name=="trigger_EjercicioPictogramas")
 			{
 				//CDG_Mundo3D.posicionPersonaje = 2;
 				destinoPortal= "ejercicioDado";
 				print(destinoPortal);
-
 			}
 			else if(coli.gameObject.name=="trigger_EjercicioSonidos")
 			{
@@ -49,6 +52,65 @@ public class controlPortales : MonoBehaviour {
 			{
 				//CDG_Mundo3D.posicionPersonaje = 2;
 				destinoPortal= "islaFantasma";
+				print(destinoPortal);
+				
+				//animator_PanelCanvas.Play("pasarABlanco");
+				//Invoke("cargarPictogramas2",100*Time.deltaTime);
+			}
+
+			//TRIGGERS ISLA FANTASMA
+			else if(coli.gameObject.name=="trigger_ejercicioSecuencias")
+			{
+				//CDG_Mundo3D.posicionPersonaje = 2;
+				destinoPortal= "ejercicioSecuencias";
+				print(destinoPortal);
+				
+				//animator_PanelCanvas.Play("pasarABlanco");
+				//Invoke("cargarPictogramas2",100*Time.deltaTime);
+			}
+			else if(coli.gameObject.name=="trigger_ejercicioCanasta")
+			{
+				//CDG_Mundo3D.posicionPersonaje = 2;
+				destinoPortal= "ejercicioCanasta";
+				print(destinoPortal);
+				
+				//animator_PanelCanvas.Play("pasarABlanco");
+				//Invoke("cargarPictogramas2",100*Time.deltaTime);
+			}
+			else if(coli.gameObject.name=="trigger_islaMecanica")
+			{
+				//CDG_Mundo3D.posicionPersonaje = 2;
+				destinoPortal= "islaMecanica";
+				print(destinoPortal);
+				
+				//animator_PanelCanvas.Play("pasarABlanco");
+				//Invoke("cargarPictogramas2",100*Time.deltaTime);
+			}
+
+
+			//TRIGGERS ISLA MECANICA
+			else if(coli.gameObject.name=="trigger_ejercicioEmocionesSocial")
+			{
+				//CDG_Mundo3D.posicionPersonaje = 2;
+				destinoPortal= "ejercicioEmocionesSocial";
+				print(destinoPortal);
+				
+				//animator_PanelCanvas.Play("pasarABlanco");
+				//Invoke("cargarPictogramas2",100*Time.deltaTime);
+			}
+			else if(coli.gameObject.name=="trigger_ejercicioEmociones")
+			{
+				//CDG_Mundo3D.posicionPersonaje = 2;
+				destinoPortal= "ejercicioEmociones";
+				print(destinoPortal);
+				
+				//animator_PanelCanvas.Play("pasarABlanco");
+				//Invoke("cargarPictogramas2",100*Time.deltaTime);
+			}
+			else if(coli.gameObject.name=="trigger_altarFinal")
+			{
+				//CDG_Mundo3D.posicionPersonaje = 2;
+				destinoPortal= "altarFinal";
 				print(destinoPortal);
 				
 				//animator_PanelCanvas.Play("pasarABlanco");
@@ -73,6 +135,8 @@ public class controlPortales : MonoBehaviour {
 	public void usarPortal(){
 		switch (destinoPortal)
 		{
+		
+		//ISLA BOSQUE
 		case "ejercicioDado":
 			Application.LoadLevel("Dado_SeleccionNivel");
 			break;
@@ -84,6 +148,35 @@ public class controlPortales : MonoBehaviour {
 			Application.LoadLevel("Isla_fantasma");
 			print ("Cargando islaFantasma...");
 			break;
+
+		//ISLA FANTASMA
+		case "ejercicioSecuencias":
+			//Application.LoadLevel("Dado_SeleccionNivel");
+			print ("Cargando ejercicio secuencias...");
+			break;
+		case "ejercicioCanasta":
+			//Application.LoadLevel("Sonidos_menu_Seleccion");
+			print ("Cargando ejercicio canasta...");
+			break;
+		case "islaMecanica":
+			//Application.LoadLevel("Isla_fantasma");
+			print ("Cargando islaMecanica...");
+			break;
+
+		//ISLA MECANICA
+		case "ejercicioEmocionesSocial":
+			//Application.LoadLevel("Dado_SeleccionNivel");
+			print ("Cargando ejercicio emociones social...");
+			break;
+		case "ejercicioEmociones":
+			//Application.LoadLevel("Sonidos_menu_Seleccion");
+			print ("Cargando ejercicio emociones...");
+			break;
+		case "altarFinal":
+			//Application.LoadLevel("Isla_fantasma");
+			print ("Cargando altarFINAL...");
+			break;
+
 
 		}
 	}
